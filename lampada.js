1d5c404a8caa5303c644de3da187de93bde8dc7f
KAMILY VITORIA TOSTA <21192995@CFP127ALUNO.LOCAL>
2021-08-25 09:18:48 -0300

    desligar

--- lampada.js
+++ lampada.js
@@ -5,10 +5,21 @@ function ligarLampada(){
     lampada.src = "img/ligada.jpg"
 }
 
+function desligarLampada(){
+    const lampada = document.getElementById("lampada")
+    lampada.src = "img/desligada.jpg"
+}
+
+
 
 
 
 // eventos 
 document.getElementById('ligar')
     .addEventListener("click", ligarLampada)
+
+document.getElementById('desligar')
+    .addEventListener("click", desligarLampada)
+
+
 
